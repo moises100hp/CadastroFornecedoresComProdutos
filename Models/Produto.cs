d5bbe94f6bc64af516aaa0e4c0280f9576609ce5
Moisés Gonçalves Silva <moises50hp@hotmail.com>
2023-03-04 20:09:00 -0300

    Ajustes

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -4,6 +4,7 @@ namespace AppMvcBasica.Models
 {
     public class Produto : Entity
     {
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public Guid FornecedorId { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
@@ -13,12 +14,12 @@ namespace AppMvcBasica.Models
         [StringLength(1000, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Descricao { get; set; }
 
-        [Required(ErrorMessage = "O campo {0} é obrigatório")]
-        public string Imagem { get; set; }
+        //[Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string Imagem { get; set; } = "";
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public decimal Valor { get; set; }
-        public DateTime DataCadastro { get; set; }
+        public DateTime DataCadastro { get; set; } = DateTime.Now;
         public bool Ativo { get; set; }
 
         /* EF Relation */
